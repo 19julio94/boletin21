2426edb97b4944d6a48d5b10e75c43764a804a80
mmoureperez <mmoureperez@atanasoff02.danielcastelao.org>
2015-10-02 12:09:18 +0200

    cambio

---
+++ src/boletin21/Boletin21.java
@@ -0,0 +1,19 @@
+
+package boletin21;
+public class Boletin21 {
+
+    public static void main(String[] args) {
+        
+    float base = 2.0f, area ; //Poño un f por ser de tipo flout, se nn saltame a double que e o principal
+    
+    float altura = (float) 3.0;// fago un cast, ponse float ou o tipo a convertir entre parentesis, se nn se pon a f.
+    
+    area = base*altura/2;
+    
+    System.out.println("area = "+area);
+    
+    
+   
+    }
+    
+}
